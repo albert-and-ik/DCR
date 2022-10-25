e0f9002e60f9686eb5c0ae700a21a4f709204cdf
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-25 10:44:03 +0300

    impl service

--- src/main/java/com/example/dcr/service/impl/CameraServiceImpl.java
+++ src/main/java/com/example/dcr/service/impl/CameraServiceImpl.java
@@ -1,9 +1,10 @@
-package com.example.dcr.service;
+package com.example.dcr.service.impl;
 
 import com.example.dcr.config.mapper.CameraMapper;
 import com.example.dcr.model.dto.CameraDto;
 import com.example.dcr.model.entity.CameraEntity;
 import com.example.dcr.repository.CameraRepository;
+import com.example.dcr.service.CameraService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +13,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class CameraServiceImpl implements CameraService{
+public class CameraServiceImpl implements CameraService {
 
     final CameraRepository cameraRepository;
     final CameraMapper mapper;
