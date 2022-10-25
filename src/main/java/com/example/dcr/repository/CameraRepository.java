7f140bb4dfe79e55197d419bbd231ea3c7fbd39d
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-25 10:42:40 +0300

    added method CameraRepository.getCameraEntitiesByFavoritesIsTrue

--- src/main/java/com/example/dcr/repository/CameraRepository.java
+++ src/main/java/com/example/dcr/repository/CameraRepository.java
@@ -3,6 +3,9 @@ package com.example.dcr.repository;
 import com.example.dcr.model.entity.CameraEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CameraRepository extends  BaseEntityRepository<CameraEntity> {
+    List<CameraEntity> getCameraEntitiesByFavoritesIsTrue();
 }
