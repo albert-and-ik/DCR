b3e4d440d68d28a7f1d2b029373560b3f1feb520
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-27 12:14:06 +0300

    added field name, replace boolean to Boolean

--- src/main/java/com/example/dcr/model/entity/CameraEntity.java
+++ src/main/java/com/example/dcr/model/entity/CameraEntity.java
@@ -4,23 +4,30 @@ package com.example.dcr.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter
 @Setter
 @Table(name= "cameras")
-public class CameraEntity extends BaseEntity{
+public class CameraEntity extends BaseEntity {
 
-    @Column(nullable = false)
-    boolean favorites;
+    @Column(name="favorites",nullable = false)
+    Boolean favorites;
 
-    @Column(nullable = false)
-    boolean rec;
+    @Column(name="rec",nullable = false)
+    Boolean rec;
 
-    @Column
+    @NotBlank
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "snapshot")
     String snapshot;
 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room")
+    RoomEntity room;
 }
