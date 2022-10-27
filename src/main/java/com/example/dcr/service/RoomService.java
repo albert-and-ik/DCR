25d5fc60274a2001fb88033a775d384961c8891d
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-27 23:43:23 +0300

    done

--- src/main/java/com/example/dcr/service/RoomService.java
+++ src/main/java/com/example/dcr/service/RoomService.java
@@ -1,19 +1,14 @@
 package com.example.dcr.service;
 
-import com.example.dcr.model.dto.CameraDto;
-import com.example.dcr.model.dto.DoorDto;
 import com.example.dcr.model.dto.RoomDto;
+import com.example.dcr.model.entity.RoomEntity;
 
 import java.util.List;
 
 public interface RoomService {
     List<RoomDto> getAll();
 
-    List<CameraDto> getCamerasFromRoom(long id);
-
-    List<DoorDto> getDoorsFromRoom(long id);
-
     void updateOrCreate(List<String> names);
 
-    long getRoomByNameOrCreate(String name);
+    RoomEntity getRoomByNameOrCreate(String name);
 }
