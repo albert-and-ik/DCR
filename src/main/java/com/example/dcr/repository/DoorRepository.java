160405890e3b34c695c6daf681791e56c5c1b6c9
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-27 16:54:38 +0300

    added methods DoorRepository.getDoorEntitiesByFavoritesIsTrue & .getDoorEntitiesByRoomId

--- src/main/java/com/example/dcr/repository/DoorRepository.java
+++ src/main/java/com/example/dcr/repository/DoorRepository.java
@@ -3,6 +3,10 @@ package com.example.dcr.repository;
 import com.example.dcr.model.entity.DoorEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DoorRepository extends BaseEntityRepository<DoorEntity>{
+    List<DoorEntity> getDoorEntitiesByFavoritesIsTrue();
+    List<DoorEntity> getDoorEntitiesByRoomId(long id);
 }
