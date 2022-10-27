b44397ef16585493604ac2e1f1361955805ad98d
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-27 16:53:15 +0300

    added CameraService.updateOrCreate

--- src/main/java/com/example/dcr/service/CameraService.java
+++ src/main/java/com/example/dcr/service/CameraService.java
@@ -1,13 +1,17 @@
 package com.example.dcr.service;
 
 import com.example.dcr.model.dto.CameraDto;
+import com.example.dcr.model.entity.CameraEntity;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CameraService {
-    List<CameraDto> getFavorites();
+    Stream<CameraEntity> getFavorites();
 
     void setFavorite(long id, boolean isFavorite);
 
     void setRec(long id, boolean isRec);
+
+    void updateOrCreate(List<CameraDto> dtos);
 }
