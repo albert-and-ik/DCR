1eec9c360f0beee479d28d8d57f76c0693031547
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-28 10:46:04 +0300

    added static method ResponseBodyDto.getFalseSuccess & ResponseBodyDto.getTrueSuccess

--- src/main/java/com/example/dcr/model/dto/ResponseBodyDto.java
+++ src/main/java/com/example/dcr/model/dto/ResponseBodyDto.java
@@ -7,4 +7,12 @@ import org.springframework.lang.NonNull;
 public class ResponseBodyDto<T> {
     @NonNull final boolean success;
     final T data;
+
+    public static <T> ResponseBodyDto<T> getTrueSuccess(T data){
+        return new ResponseBodyDto<>(true, data);
+    }
+
+    public static <T> ResponseBodyDto<T> getFalseSuccess(T data){
+        return new ResponseBodyDto<>(false, data);
+    }
 }
