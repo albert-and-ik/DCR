a4fa9d50187186957265bdf7a01ced4a5399ba7a
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-28 09:38:14 +0300

    deleted dependency MapperStruct

--- src/main/java/com/example/dcr/service/impl/CameraServiceImpl.java
+++ src/main/java/com/example/dcr/service/impl/CameraServiceImpl.java
@@ -1,22 +1,24 @@
 package com.example.dcr.service.impl;
 
-import com.example.dcr.config.mapper.CameraMapper;
 import com.example.dcr.model.dto.CameraDto;
 import com.example.dcr.model.entity.CameraEntity;
 import com.example.dcr.repository.CameraRepository;
 import com.example.dcr.service.CameraService;
+import com.example.dcr.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Service
 @AllArgsConstructor
 public class CameraServiceImpl implements CameraService {
 
     final CameraRepository cameraRepository;
-    final CameraMapper mapper;
+    final RoomService roomService;
 
     @Override
     @Transactional(readOnly = true)
@@ -24,9 +26,10 @@ public class CameraServiceImpl implements CameraService {
         return cameraRepository
                 .getCameraEntitiesByFavoritesIsTrue()
                 .stream()
-                .map(mapper::toDto)
+                .map(CameraDto::toDto)
                 .toList();
     }
+
 
     @Override
     @Transactional
@@ -50,6 +53,44 @@ public class CameraServiceImpl implements CameraService {
         entity.setRec(isRec);
 
         cameraRepository.save(entity);
+
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<CameraDto> getCamerasFromRoom(String name) {
+        return cameraRepository
+                .getCameraEntitiesByRoomName(name)
+                .stream()
+                .map(CameraDto::toDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public void updateOrCreate(List<CameraDto> dtos) {
+
+        LocalDateTime updateTime = LocalDateTime.now();
+
+        var listEntities = dtos
+                .stream()
+                .map(dto -> {
+                    var cameraEntity = dto.toEntity();
+
+                    if(cameraRepository.existsById(Long.parseLong(dto.getId()))) {
+                        cameraEntity.setFavorites(null);
+                        cameraEntity.setRec(null);
+                    }
+
+                    cameraEntity.setRoom(roomService.getRoomByNameOrCreate(dto.getRoom()));
+
+                    cameraEntity.setUpdatedAt(updateTime);
+
+                    return cameraEntity;
+                })
+                .toList();
+
+        cameraRepository.saveAll(listEntities);
 
     }
 }
