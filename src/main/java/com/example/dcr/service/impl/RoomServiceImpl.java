2dd6f53a418bae34eedb307583bbb01108405377
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-28 09:33:47 +0300

    update RoomServiceImpl.getRoomByNameOrCreate

--- src/main/java/com/example/dcr/service/impl/RoomServiceImpl.java
+++ src/main/java/com/example/dcr/service/impl/RoomServiceImpl.java
@@ -1,15 +1,12 @@
 package com.example.dcr.service.impl;
 
-import com.example.dcr.mapper.EntityMapper;
-import com.example.dcr.model.dto.CameraDto;
-import com.example.dcr.model.dto.DoorDto;
 import com.example.dcr.model.dto.RoomDto;
 import com.example.dcr.model.entity.RoomEntity;
 import com.example.dcr.repository.CameraRepository;
 import com.example.dcr.repository.RoomRepository;
-import com.example.dcr.service.DoorService;
 import com.example.dcr.service.RoomService;
 import lombok.AllArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +18,7 @@ import java.util.List;
 public class RoomServiceImpl implements RoomService {
 
     final RoomRepository roomRepository;
-    final DoorService doorService;
     final CameraRepository cameraRepository;
-    final EntityMapper mapper;
 
     @Override
     @Transactional(readOnly = true)
@@ -31,23 +26,8 @@ public class RoomServiceImpl implements RoomService {
         return roomRepository
                 .findAll()
                 .stream()
-                .map(mapper::toDto)
+                .map(RoomDto::toDto)
                 .toList();
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<CameraDto> getCamerasFromRoom(long id) {
-        return cameraRepository
-                .getCameraEntitiesByRoomId(id)
-                .stream()
-                .map(mapper::toDto)
-                .toList();
-    }
-
-    @Override
-    public List<DoorDto> getDoorsFromRoom(long id) {
-        return doorService.getDoorEntitiesByRoomId(id);
     }
 
     @Override
@@ -56,20 +36,19 @@ public class RoomServiceImpl implements RoomService {
         names.forEach(this::getRoomByNameOrCreate);
     }
 
+
     @Override
     @Transactional
-    public long getRoomByNameOrCreate(String name) {
-        long idRoom = roomRepository.findFirstByName(name);
+    public @Nullable RoomEntity getRoomByNameOrCreate(String name) {
+        if(name == null || name.equals("null") || name.isEmpty())
+            return null;
 
-        if(idRoom<=0){
-            var roomEntity = new RoomEntity();
-            roomEntity.setId(roomRepository.getMaximumId()+1);
-            roomEntity.setName(name);
-            roomEntity.setUpdatedAt(LocalDateTime.now());
-            return roomRepository.save(roomEntity).getId();
-        }else {
-            return idRoom;
-        }
-
+        return roomRepository
+                .findById(name)
+                .orElseGet(() -> {
+                    var newRoom = new RoomEntity(name);
+                    newRoom.setUpdatedAt(LocalDateTime.now());
+                    return roomRepository.save(newRoom);
+                });
     }
 }
