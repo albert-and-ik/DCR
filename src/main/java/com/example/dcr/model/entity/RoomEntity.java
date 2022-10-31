8df3a0aed0f1ba5eb926b49d4b9757190613684a
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-31 12:26:57 +0300

    added extends BaseEmptyEntity

--- src/main/java/com/example/dcr/model/entity/RoomEntity.java
+++ src/main/java/com/example/dcr/model/entity/RoomEntity.java
@@ -2,10 +2,12 @@ package com.example.dcr.model.entity;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -13,9 +15,13 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @Entity
 @Table(name = "rooms")
-public class RoomEntity extends BaseEntity{
-
-    @NotBlank
+@NoArgsConstructor
+public class RoomEntity extends BaseEmptyEntity{
+    @Id
     @Column(name="name")
     String name;
+
+    public RoomEntity(@NotBlank String name){
+        this.name = name;
+    }
 }
